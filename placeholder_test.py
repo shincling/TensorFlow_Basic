70bde8a025a0f9495e6b51fccfcb01786c16169b
shincling <shijing609@163.com>
2017-05-12 19:02:48 +0800

    run, fetch 输出和 feed输入测试

--- placeholder_test.py
+++ placeholder_test.py
@@ -5,7 +5,7 @@ input2=tf.placeholder(tf.float32)
 option=tf.multiply(input1,input2)
 
 with tf.Session() as sess:
-    result=sess.run(option,feed_dict={input1:[7],input2:3})
+    result=sess.run([option,input1,input2],feed_dict={input1:7.,input2:3.})
     print type(result)
     print result
 
